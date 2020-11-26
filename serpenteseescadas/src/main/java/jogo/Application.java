557456102b6d3aff3170cbce4773d13e1c2aceb5
Ezandro Bueno <57026708+ezbueno@users.noreply.github.com>
2020-11-26 19:06:35 -0300

    Projeto finalizado

---
+++ serpenteseescadas/src/main/java/jogo/Application.java
@@ -0,0 +1,12 @@
+package jogo;
+
+import jogo.core.Game;
+
+public class Application {
+
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.play();
+	}
+
+}
---
+++ serpenteseescadas/src/main/java/jogo/infrastructure/Printable.java
@@ -0,0 +1,6 @@
+package jogo.infrastructure;
+
+public interface Printable {
+
+	public void print();
+}
